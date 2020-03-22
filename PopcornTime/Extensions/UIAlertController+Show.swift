004917da5a4a0ba449081fe71a01964d9cbe94bd
Maxim Antonov <xpcgamer@gmail.com>
2020-03-22 18:49:05 +0000

    fix conflicts

--- PopcornTime/Extensions/UIAlertController+Show.swift
+++ PopcornTime/Extensions/UIAlertController+Show.swift
@@ -4,25 +4,6 @@ import Foundation
 import UIKit.UIAlertController
 
 extension UIAlertController {
-
-    private struct AssociatedKey {
-       static var window:   UInt8 = 0
-    }
-
-    var window: UIWindow? {
-        get {
-          return objc_getAssociatedObject(self, &AssociatedKey.window) as? UIWindow
-        }
-        set {
-            objc_setAssociatedObject(self, &AssociatedKey.window, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
-    }
-
-    open override func viewDidDisappear(_ animated: Bool) {
-      super.viewDidDisappear(animated)
-      window?.isHidden = true
-      window = nil
-    }
     
     private struct AssociatedKey {
           static var window:   UInt8 = 0
